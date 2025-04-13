bf5a571b0236dbf04354ec39cf668a5aa373484f
ddu0422 <ddu0422@naver.com>
2025-04-14 02:01:08 +0900

    feat: 계좌번호 추가

---
+++ js/account_number.js
@@ -0,0 +1,14 @@
+function copyAccountNumber(id) {
+  const accountText = document.getElementById(id).innerText;
+  navigator.clipboard.writeText(accountText).then(() => {
+    showToast();
+  });
+}
+
+function showToast() {
+  const toast = document.getElementById("copy_toast");
+  toast.style.display = "block";
+  setTimeout(() => {
+    toast.style.display = "none";
+  }, 2000);
+}
